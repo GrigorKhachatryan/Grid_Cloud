580f481fa157d21dbf08bf4cb3912cbb82bd9599
Григор <43500991+GrigorKhachatryan@users.noreply.github.com>
2019-12-15 13:41:35 +0300

    Delete constant.py

--- FlaskApp/constant.py
+++
@@ -1,9 +0,0 @@
-
-host = 'ec2-50-19-95-77.compute-1.amazonaws.com'
-name = 'deh9d750n9cker'
-user = 'locvyqlaluwugc'
-password = 'ff9890fca56998a2f445236fbb10d4082a13883b7daa7cacc103b03048c4ab6b'
-us = 'grigor280398@yandex.ru'
-pas = 'Grigor123'
-
-
